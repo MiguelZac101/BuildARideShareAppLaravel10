4dbcd2cda136610cc90ccda6e09946d1cedcf201
zack <miguel.quispe@fractal.com.pe>
2026-02-27 07:01:37 -0500

    driver show update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[LoginController::class,'submit']);
 Route::post('/login/verify',[LoginController::class,'verify']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Protected routes that require authentication
+    Route::get('driver', [DriverController::class, 'show']);
+    Route::post('driver', [DriverController::class, 'update']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
